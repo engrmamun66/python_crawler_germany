a9c63b82afdfea82779c59c1118bb73f8d9db195
Arnavbd1971 <shivbd1971@gmail.com>
2022-01-25 17:18:38 +0600

    commit

--- crawler.py
+++ crawler.py
@@ -35,8 +35,8 @@ def read_ads(input_keyword,driver):
     id_list = []
     rank = 0
     #options.add_argument('headless')
-    # driver.set_window_size(700, 1080) # set window size to 700*1080 pixel
-    driver.maximize_window()
+    driver.set_window_size(700, 1080) # set window size to 700*1080 pixel
+    # driver.maximize_window()
     # CRAWL GOOGLE -------------------------------------------------------------------------------------------------------------------------
     # navigate to Google website and accept cookies
     driver.get("https://www.google.de/search?q={}".format(input_keyword))
