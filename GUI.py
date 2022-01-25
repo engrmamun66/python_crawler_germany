9e65e0e24db44824f6ae1d145ac6b6c35b24b445
Arnavbd1971 <shivbd1971@gmail.com>
2022-01-25 17:58:08 +0600

    commit

--- GUI.py
+++ GUI.py
@@ -49,8 +49,8 @@ class GUI():
                     s=Service(PATH)
                     options = webdriver.ChromeOptions()
                     options.add_experimental_option('excludeSwitches', ['enable-automation'])
-                    driver = webdriver.Chrome(executable_path=Service(PATH), chrome_options=options)
-                    # driver = webdriver.Chrome(service=s)
+                    # driver = webdriver.Chrome(executable_path=PATH, chrome_options=options)
+                    driver = webdriver.Chrome(service=s)
                     # driver = webdriver.Chrome(service=s, chrome_options=options)
                     try:
                         results[keyword] = crawler.read_ads(keyword,driver)
