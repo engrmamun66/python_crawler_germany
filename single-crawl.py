1483686f2b935a90bc69be0fd2fd0441a486717d
engrmamun97 <engrmamun97@gmail.com>
2022-01-31 08:43:17 +0600

    prepare to start with exe

--- single-crawl.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/results?search_query=e-scooter");
-assert "Chrome" in driver.title
-elem = driver.find_elements_by_name("h1")
-
-print(len(elem))
